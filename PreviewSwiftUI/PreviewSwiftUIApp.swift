8c291474c9609ef2d9f72932d17256fb9812141f
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-17 19:07:32 +0300

    Initial Commit

---
+++ PreviewSwiftUI/PreviewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewSwiftUIApp.swift
+//  PreviewSwiftUI
+//
+//  Created by İbrahim Halid Bayrak on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct PreviewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
